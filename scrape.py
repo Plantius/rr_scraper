01b723354538c5f4576e6c62a1e3d943842ebd37
niels <niels>
2024-04-11 12:30:50 +0200

    Reworked chapter list scraper

--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 import requests, subprocess
 from bs4 import BeautifulSoup
-import sys, numpy as np, re
+import sys, numpy as np, json
 
 args = sys.argv
 if len(args) < 2:
@@ -31,22 +31,14 @@ def getList(url):
     if temp is None:
         print("No page found.")
         exit(1)
-    temp = temp[temp.find("[")+1:temp.find("]")]
-    
-    return [[[s for s in sp.split(':')] for sp in chapter.split(',')] for chapter in temp[1:-1].split('},{')]
+        
+    return [json.loads(dic) for dic in ['{'+r+'}' for r in temp[temp.find("[")+1:temp.find("]")].split("},{")[1:-1]]]
 
-def convertList(chapter_list, num_chapters):
-    for i in np.arange(len(chapter_list)):
-        dict_chapters = {}
-
-        if i >= num_chapters:
-            break
-
-        for item in chapter_list[i]:
-            dict_chapters[item[0].replace('"', '')] = item[1].replace('"', '')
-        chapter_list[i] = dict_chapters
-    
+def convertList(chapter_list, num_chapters):    
+    count = 0
     for chapter in chapter_list:
+        if count >= num_chapters:
+            break
         page = getPage(f"{rr}{chapter['url']}")
         
         for tag in tagRemove:
@@ -54,17 +46,25 @@ def convertList(chapter_list, num_chapters):
                 item.replaceWithChildren()
         
         con = page.find("div", class_="chapter-inner chapter-content")
-        stripped_con = [line for line in con.contents if line is not None and line != "\u200b" and line != u"\xa0"]
+        for item in con.findAll("div"):
+            item.replaceWithChildren()
+        stripped_con = [line for line in con.contents if line is not None]
         str_con = ''.join(str(line) for line in stripped_con)
 
-        chapter["chapter_content"] = str_con.replace("\u200b", "").replace(u"\xa0", "").replace("\n", "\\par\n").replace("%", "\\%").replace("#", "\\#").replace("&", "\\&").replace("<strong>", "\\textbf{").replace("</strong>", "}").replace("\\&gt;", "\\textgreater").replace("\\&lt;", "\\textless").replace("The author\'s content has been appropriated; report any instances of this story on Amazon.", "")
+        chapter["chapter_content"] = str_con.replace("$", "").replace("\u200b", "").replace(u"\xa0", "").replace("\n", "\\par\n").replace("%", "\\%").replace("#", "\\#").replace("&", "\\&").replace("<strong>", "\\textbf{").replace("</strong>", "}").replace("\\&gt;", "\\textgreater").replace("\\&lt;", "\\textless").replace("The author\'s content has been appropriated; report any instances of this story on Amazon.", "").replace("Taken from Royal Road, this narrative should be reported if found on Amazon.", "")
+        count += 1
+
     return chapter_list
 
-def createLaTeX(chapter_list):
+def createLaTeX(chapter_list, num_chapters):
     latex = [t.replace("TITLE", title[0]) for t in np.loadtxt("latex_template.tex", dtype=str)]
+    count = 0
     for chapter in chapter_list:
+        if count >= num_chapters:
+            break
         latex.insert(-1, f'\\section*{{{chapter["title"]}}}\n\\addcontentsline{{toc}}{{section}}{{\\protect\\numberline{{}}{chapter["title"]}}}%')
         latex.insert(-1, f'{chapter["chapter_content"]}\n')
+        count += 1
     
     file = open(f"{title[0]}.tex", 'w')
     for line in latex:
@@ -79,10 +79,10 @@ def createPDF(filename):
 temp = getList(url)
 
 if len(args) >= 3:
-    num_chap = args[2]
+    num_chap = int(args[2])
 else:
     num_chap = len(temp)
 
 chapter_list = convertList(temp, num_chap)
-createLaTeX(chapter_list)
+createLaTeX(chapter_list, num_chap)
 createPDF(title[0])
